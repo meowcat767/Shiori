5aaa6aaf7516f5624efb236df3c3c994ef9cad8d
Ben <meowcat@meowcat.site>
2026-02-07 10:32:12 +0000

    implement RPC service

---
+++ src/main/java/services/DiscordRPCService.java
@@ -0,0 +1,41 @@
+package services;
+
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.activity.Activity;
+
+import java.time.Instant;
+import java.util.concurrent.*;
+
+public class DiscordRPCService {
+    private Core core;
+    private ScheduledExecutorService callBackExecutor;
+    private long clientId = 1402751935466963214L;
+
+    public void start(long clientId) {
+        CreateParams params = new CreateParams();
+        params.setClientID(clientId);
+        params.setFlags(CreateParams.getDefaultFlags());
+        core = new Core(params);
+        callBackExecutor = Executors.newSingleThreadScheduledExecutor();
+        callBackExecutor.scheduleAtFixedRate(
+                core::runCallbacks,
+                0,
+                10,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    public void initRpc() {
+        try(Activity activity = new Activity()) {
+            activity.setDetails("Reading with Yomikomu");
+            activity.timestamps().setStart(Instant.now());
+            core.activityManager().updateActivity(activity);
+        }
+    }
+    public void stop() {
+        callBackExecutor.shutdownNow();
+        core.close();
+    }
+
+}
