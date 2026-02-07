a75393b1db130a4ecadd25c872ef9aeec938366e
Ben <meowcat@meowcat.site>
2026-02-07 09:54:03 +0000

    Add Discord Social SDK to Maven

--- src/main/java/exception/UhOhPythonDied.java
+++ src/main/java/exception/UhOhPythonDied.java
@@ -2,6 +2,9 @@ package exception;
 
 import static java.lang.System.err;
 
+/**
+ * This should be used in a execpt where Python is involved.
+ * */
 public class UhOhPythonDied extends RuntimeException {
     public UhOhPythonDied(String message, Throwable err) {
         super(message, err);
